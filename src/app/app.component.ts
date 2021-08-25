781eee88fd0436c6032d0446f476f260cfa644d7
Penchala Babu <penchalababuchalla@gmail.com>
2021-08-25 13:48:39 +0530

    new project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,49 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'game-project';
+  lastValue = "e";
+  isdisabled = false;
+
+  matrix = [
+    ["e", "e", "e"],
+    ["e", "e", "e"],
+    ["e", "e", "e"]
+  ];
+
+  changeValue(i: number, j: number){
+    
+
+      if(this.lastValue == "e") {
+        this.matrix[i][j] ='0';
+        this.lastValue = '0'
+        // this.isdisabled = true;
+      } else if (this.lastValue == "0") {
+        this.matrix[i][j] ='X';
+        this.lastValue = 'X'
+        // this.isdisabled = true;
+      }else if (this.lastValue == "X") {
+        this.matrix[i][j] ='0';
+        this.lastValue = '0'
+        // this.isdisabled = true;
+      }
+      if(this.matrix[0][0]=='0'&&this.matrix[0][1]=='0'&&this.matrix[0][2]=='0'){
+      alert("gameOver");
+      return;
+    }
+    if(this.matrix[0][0]=='X'&&this.matrix[0][1]=='X'&&this.matrix[0][2]=='X'){
+      alert("gameOver");
+      return;
+    }
+    if(this.matrix[0][0]=='0'&&this.matrix[1][1]=='0'&&this.matrix[2][2]=='0'){
+      alert("gameOver");
+      return;
+    }
+    if(this.matrix[0][0]=='X'&&this.matrix[1][1]=='X'&&this.matrix[2][2]=='X'){
+      alert("gameOver")
+      return;
+    }
+     
+  }
 }
