9e30ebf82c2a69cba7dfe234fbb47a736a56a665
Penchala Babu <penchalababuchalla@gmail.com>
2021-08-26 12:49:13 +0530

    css change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,10 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   lastValue = "e";
-  isdisabled = false;
+  isdisable1 = false;
   isShow = true;
+  winner1 = false;
+  winner2 = false;
 
   matrix = [
     ["e", "e", "e"],
@@ -17,8 +19,21 @@ export class AppComponent {
     ["e", "e", "e"]
   ];
 
+  disableMatrix = [
+    [false, false, false],
+    [false, false, false],
+    [false, false, false]
+  ];
+
   openGame(){
     this.isShow = false;
+  }
+
+  hideGameLayout(){
+    if(this.winner1 || this.winner2) {
+      return true
+    }
+    return this.isShow;
   }
 
 
@@ -28,32 +43,62 @@ export class AppComponent {
       if(this.lastValue == "e") {
         this.matrix[i][j] ='0';
         this.lastValue = '0'
-        // this.isdisabled = true;
+        this.disableMatrix[i][j] = true;
+       
+
       } else if (this.lastValue == "0") {
         this.matrix[i][j] ='X';
         this.lastValue = 'X'
-        // this.isdisabled = true;
+        this.disableMatrix[i][j] = true;
+        
       }else if (this.lastValue == "X") {
         this.matrix[i][j] ='0';
         this.lastValue = '0'
-        // this.isdisabled = true;
+        this.disableMatrix[i][j] = true;
+       
       }
       if(this.matrix[0][0]=='0'&&this.matrix[0][1]=='0'&&this.matrix[0][2]=='0'){
-      alert("gameOver");
-      return;
-    }
-    if(this.matrix[0][0]=='X'&&this.matrix[0][1]=='X'&&this.matrix[0][2]=='X'){
-      alert("gameOver");
-      return;
-    }
-    if(this.matrix[0][0]=='0'&&this.matrix[1][1]=='0'&&this.matrix[2][2]=='0'){
-      alert("gameOver");
-      return;
-    }
-    if(this.matrix[0][0]=='X'&&this.matrix[1][1]=='X'&&this.matrix[2][2]=='X'){
-      alert("gameOver")
-      return;
-    }
-     
+        this.winner1 = true;
+      }
+      if(this.matrix[0][0]=='X'&&this.matrix[0][1]=='X'&&this.matrix[0][2]=='X'){
+        this.winner2 = true;
+      }
+      if(this.matrix[1][0]=='0'&&this.matrix[1][1]=='0'&&this.matrix[1][2]=='0'){
+        this.winner1 = true;
+      }
+      if(this.matrix[1][0]=='X'&&this.matrix[1][1]=='X'&&this.matrix[1][2]=='X'){
+        this.winner2 = true;
+      }
+      if(this.matrix[2][0]=='0'&&this.matrix[2][1]=='0'&&this.matrix[2][2]=='0'){
+        this.winner1 = true;
+      }
+      if(this.matrix[2][0]=='X'&&this.matrix[2][1]=='X'&&this.matrix[2][2]=='X'){
+        this.winner2 = true;
+      }
+      if(this.matrix[0][0]=='0'&&this.matrix[1][0]=='0'&&this.matrix[2][0]=='0'){
+        this.winner1 = true;
+      }
+      if(this.matrix[0][0]=='X'&&this.matrix[1][0]=='X'&&this.matrix[2][0]=='X'){
+        this.winner2 = true;
+      }
+      if(this.matrix[0][1]=='0'&&this.matrix[1][1]=='0'&&this.matrix[2][1]=='0'){
+        this.winner1 = true;
+      }
+      if(this.matrix[0][2]=='X'&&this.matrix[1][2]=='X'&&this.matrix[2][2]=='X'){
+        this.winner2 = true;
+      }
+      if(this.matrix[0][2]=='0'&&this.matrix[1][2]=='0'&&this.matrix[2][2]=='0'){
+        this.winner1 = true;
+      }
+      if(this.matrix[0][0]=='X'&&this.matrix[1][1]=='X'&&this.matrix[2][2]=='X'){
+        this.winner2 = true;
+      }
+      if(this.matrix[0][0]=='0'&&this.matrix[1][1]=='0'&&this.matrix[2][2]=='0'){
+        this.winner1 = true;
+      }
+      if(this.matrix[0][0]=='X'&&this.matrix[1][1]=='X'&&this.matrix[2][2]=='X'){
+        this.winner2 = true;
+      }
+       
   }
 }
